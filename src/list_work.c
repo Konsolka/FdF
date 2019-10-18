58dbc066a268010067636d104facf6d4d0f73eb2
Alexandr <Konsolka@users.noreply.github.com>
2019-10-18 15:46:49 +0300

    adding into list

--- src/list_work.c
+++ src/list_work.c
@@ -1,34 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fdf.h                                              :+:      :+:    :+:   */
+/*   list_work.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mburl <mburl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/18 12:07:18 by mburl             #+#    #+#             */
-/*   Updated: 2019/10/18 15:32:01 by mburl            ###   ########.fr       */
+/*   Created: 2019/10/18 15:18:18 by mburl             #+#    #+#             */
+/*   Updated: 2019/10/18 15:35:19 by mburl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FDF_H
-# define FDF_H
+#include "fdf.h"
 
-# include <stdio.h>
-# include <sys/types.h>
-# include <sys/uio.h>
-# include <stdlib.h>
-# include <fcntl.h>
-# include <stdio.h>
-# include <unistd.h>
-# include "libft.h"
-# include "mlx.h"
-# include "get_next_line.h"
-
-typedef struct		s_fdf
+void	ft_lstprint(t_list *lst)
 {
-	int			*coords;
-	int				y;
-}					t_fdf;
-
-
-#endif
+	while (lst != NULL)
+	{
+		ft_putstr("lst->content = ");
+		ft_putendl((int *)lst->content);
+		lst = lst->next;
+	}
+}
--- src/main.c
+++ src/main.c
@@ -6,47 +6,67 @@
 /*   By: mburl <mburl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/18 11:07:21 by mburl             #+#    #+#             */
-/*   Updated: 2019/10/18 13:28:00 by mburl            ###   ########.fr       */
+/*   Updated: 2019/10/18 15:38:49 by mburl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-#include "libft.h"
-#include "mlx.h"
-#include <fcntl.h>
-#include <stdlib.h>
-#include "get_next_line.h"
-#include <stdio.h>
 
-int		read_file(int fd)
+t_list		*read_file(int fd)
 {
-	char *line;
+	char	*line;
 	char	**coord;
-	void	*mlx_ptr;
-	void	*win_ptr;
-	int		x;
+	t_list	*lst;
+	t_fdf	*node;
+	int		size;
 	int		y;
+	int 	i;
+	int		*values;
 
-	y = 100;
-	x = 50;
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "fdf");
+	lst = NULL;
 	line = NULL;
+	y = 0;
 	while (get_next_line(fd, &line))
 	{
-		coord = ft_strsplit(line, ' '); // функция для подсчета элементов в **coord
+		coord = ft_strsplit(line, ' ');
+		size = 0;
+		while (1)
+		{
+			if (coord[size] == NULL)
+				break;
+			size++;
+		}
+		i = 0;
+		values = (int *)malloc(sizeof(int) * size);
+		while (i < size)
+		{
+			values[i] = ft_atoi(coord[i]);
+			i++;
+		}
+		node = (t_fdf *)malloc(sizeof(t_fdf));
+		node->coords = values;
+		node->y = y;
+		ft_lstadd(&lst, ft_lstnew(node, sizeof(*node)));
+		ft_memdel((void **)node);
+		y++;
 	}
-	mlx_loop(mlx_ptr);
-	return (0);
+	return (lst);
 }
 
-int		main(int ac, char **av)
+int			print_map(t_list *lst)
+{
+	
+}
+
+int			main(int ac, char **av)
 {
 	int		fd;
-
+	t_list	*lst;
+	
 	if (ac != 2)
 		ft_putstr_err("Usage : ./fdf <filename> [ case_size z_size ]\n");
-	if ((fd = read_file(open(av[1], O_RDONLY))) == -1)
+	if ((lst = read_file(open(av[1], O_RDONLY))) == NULL)
 		ft_putstr_err("error\n");	
+	print_map(lst);
 	return (0);
 }
